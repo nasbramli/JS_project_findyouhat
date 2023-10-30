0bc9e578d73c4e85751cba259ba40a9e9dfa382d
Nasruddin Islam Bin Ramli <99019949+nasbramli@users.noreply.github.com>
2023-10-31 01:12:39 +0800

    Add files via upload

--- findHat.js
+++ findHat.js
@@ -1,9 +1,9 @@
 const prompt = require('prompt-sync')({sigint: true})
 
-const hat = '^'//hat to find
-const hole='0'//the hole
-const field = '1'//the field of play
-const path = '*'//path it draws
+const hat = '^'// object: hat to find
+const hole='0'// object: the hole
+const field = '1'// object: the field of play
+const path = '*'// object: path it draws
 
 class fields{
     constructor(field){
@@ -14,18 +14,19 @@ class fields{
         this.OutBound = false
         this.hatFound = false
 
-        const generateField = prompt('Do you want to play?'+'\n'+'(y/n)')
-        const answer = generateField.toLowerCase()
+        const generateField = prompt('Do you want to play?'+'\n'+'(y/n)').toLowerCase()
 
-        if(answer === 'y'){
+        if(generateField === 'y'){
             const xAxis = prompt('X-axis length(Please enter a number):')
             const yAxis = prompt('Y-axis height(Please enter a number):')
             const lobang =  prompt('Number of holes?:')
-
+            
+            //created the field because of adding userMove() to call for gameplay()
             this._field = this.generate(xAxis, yAxis, lobang)
 
             console.log('Controls')
 
+            //calling field for this gameplay
             this.gamePlay(this._field);
         }else{
             console.log('Maybe next time')
@@ -39,13 +40,15 @@ class fields{
             console.log(this._field[i].join(''));
         }
     }
-
+    //generating the field
     generate(height, length, noOfHoles){
         let newField = []
         let fieldElements = []
         const holeNumber = noOfHoles;
+        //creating the field of play.
         for(let i=0; i<length; i++){
             for(let j=0; j<height; j++){
+            //Position of the first *
               if( i === 0 && j === 0){
                 fieldElements.push(path)
               }else{
@@ -53,10 +56,13 @@ class fields{
               }
             }
             newField.push(fieldElements)
+            //creating a new line for field elements
             fieldElements =[]
         }
+        //adding holes during gameplay
         let holes = []
         while(holes.length < holeNumber){
+            //random holes to be implemented
             let randomLength = Math.floor(Math.random()*length)
             let randomHeight = Math.floor(Math.random()*height)
             if(randomLength !==0 || randomHeight !==0){
@@ -68,6 +74,7 @@ class fields{
         }
         let hatSeek = false
 
+        //adding hat at a random place
         while (hatSeek == false){
             let x = Math.floor(Math.random()*length)
             let y = Math.floor(Math.random()*height)
@@ -82,32 +89,37 @@ class fields{
     }
 
     userMove(){
-        const direction = prompt('Your move: ')
+        const direction = prompt('Your move: ').toLowerCase()
 
+        //directions key
         if(direction ==='d'){
+            //+1 moves right one step
             this._xaxis +=1;
         }else if(direction ==='s'){
+            //+1 moves left one step
             this._xaxis -=1;
         }else if(direction ==='e'){
+            //-1 moves up one step
             this._yaxis -=1
         }else if(direction ==='x'){
+            //moves down one step
             this._yaxis +=1
         }else{
             console.log('Invalid. try again')
             this.userMove()
         }
-        if(
-            this._yaxis<0 ||
-            this._yaxis> this._field.length-1 ||
-            this._xaxis> this._field[0].length-1
-        ){
+        //conditions for user move on out of bounds
+        if( this._yaxis<0 || this._yaxis> this._field.length-1 || this._xaxis> this._field[0].length-1){
             this.outBounds = true
         }else{
+            //conditions for user move into the hole
             if(this._field[this._yaxis][this._xaxis]==='0'){
                 this.gameOver = true
+            //conditions for user move if hat found
             }else if(this._field[this._yaxis][this._xaxis]==='^'){
                 this.hatFound = true
             }else{
+                //else it is just the path user chooses to move.
                 this._field[this._yaxis][this._xaxis] = '*'
             }
         }
@@ -116,14 +128,8 @@ class fields{
     gamePlay(){
         this.print(this._field)
         this.userMove()
-        while(
-            this._xaxis >= 0&&
-            this._yaxis >= 0&&
-            this._yaxis < this._field.length &&
-            this._xaxis < this._field[0].length &&
-            this.gameOver === false &&
-            this.hatFound === false
-        ){
+        //directions of play by user
+        while(this._xaxis >= 0 && this._yaxis >= 0 && this._yaxis < this._field.length && this._xaxis < this._field[0].length && this.gameOver === false && this.hatFound === false){
             this.print()
             this.userMove()
         }
@@ -133,9 +139,7 @@ class fields{
             console.log('Out of bounds')
         }else if(this.gameOver === true){
             console.log('You landed in a hole!')
-        }/*else{
-            console.log('Game Over')
-        }*/
+        }
     }
 }
 
